8cf01d017483df84c31a1072fc248b4d8adf9c14
RYSIO2115\filca <filc.aleksandra@gmail.com>
2024-04-23 21:58:59 +0200

    Blendspace

--- Test/Source/Test/Private/Core/TestAnimInstance.cpp
+++ Test/Source/Test/Private/Core/TestAnimInstance.cpp
@@ -2,26 +2,37 @@
 
 
 #include "Core/TestAnimInstance.h"
+#include "Kismet/KismetMathLibrary.h"
 
 void UTestAnimInstance::NativeBeginPlay()
 {
 	Pawn = TryGetPawnOwner();
+	Rotation = Pawn->GetActorRotation();
+	Forward = Pawn->GetActorForwardVector();
+	Right = Pawn->GetActorRightVector();
 }
 
 void UTestAnimInstance::NativeUpdateAnimation(float DeltaSeconds)
 {
+	
 	if (IsValid(Pawn))
 	{
 		Velocity = Pawn->GetVelocity();
 		Speed = Velocity.Length();
 
-		if (Speed > 0)
-		{
-			bIsMoving = true;
-		}
-		else
-		{
-			bIsMoving = false;
-		}
+		bIsMoving = Speed > SMALL_NUMBER;
+
+		const float Dot = Velocity.GetSafeNormal().Dot(Forward);
+		//const float Dotv2 = Right.GetSafeNormal().Dot(Forward);
+		bIsMovingForward = Dot > SMALL_NUMBER;
+
+		Orientation = CalculateDirection(Velocity, Rotation);
+
+		//Orientation = ((acos(Dotv2 / (Forward.Length() * Rotation.Length())))*180)/mathPi;
+
+	}
+	else
+	{
+		Pawn = TryGetPawnOwner();
 	}
 }
--- Test/Source/Test/Public/Core/TestAnimInstance.h
+++ Test/Source/Test/Public/Core/TestAnimInstance.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "Animation/AnimInstance.h"
+#include "Math/UnrealMathUtility.h"
 #include "TestAnimInstance.generated.h"
 
 /**
@@ -14,19 +15,37 @@ class TEST_API UTestAnimInstance : public UAnimInstance
 {
 	GENERATED_BODY()
 
-public:
-	UPROPERTY(BlueprintReadOnly)
+	UPROPERTY(BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
 	FVector Velocity;
 
-	UPROPERTY(BlueprintReadOnly)
+	UPROPERTY(BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+	FRotator Rotation;
+
+	UPROPERTY(BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+	FVector Right;
+
+	UPROPERTY(BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+	FVector Forward;
+
+	UPROPERTY(BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
 	float Speed = 0.0f;
 	
-	UPROPERTY(BlueprintReadOnly)
+	UPROPERTY(BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
 	bool bIsMoving = false;
 	
-	UPROPERTY(BlueprintReadOnly)
+	UPROPERTY(BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
 	TObjectPtr<const APawn> Pawn;
 
+	UPROPERTY(BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+	bool bIsMovingForward = true;
+
+	UPROPERTY(BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+	float mathPi = PI;
+
+	UPROPERTY(BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+	float Orientation = 0.0f;
+
+protected:
 	virtual void NativeBeginPlay() override;
 	virtual void NativeUpdateAnimation(float DeltaSeconds) override;
 
